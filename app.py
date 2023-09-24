0ae02900684f02558313638413fe2ec97e39ff52
Aditya Kumar <64283433+Aditya-kumar-147@users.noreply.github.com>
2023-09-24 12:37:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from multiprocessing.dummy import Value
 import streamlit as st
 import pickle
 
-# Streamlit app Random forest
+# Streamlit app
 
 random_model=pickle.load(open('log_model.pkl','rb'))
 
@@ -14,28 +15,20 @@ def classify(num):
 def main():
 
     html_temp = """
-    <div style="background-color:teal ; padding:10px">
-    <h2 style="color:white; text-align:center;">CVD Prediction </h2>
+    <div style="background-color:teal ; padding:10px; border-radius: 5px;border-style: solid;margin-bottom: 30px;">
+    <h2 style="color:#FFB741; text-align:center;">CVD Predictor Plus</h2>
+    <h4 style="color:#D7D7D7; text-align:center;">"Heart Health in Your Hands: Predict, Prevent, Prevail!"</h4>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-
-    tem2 = """
-    <style>
-    body {
-        background-color: green; 
-    }
-    </style>
-    """
-    st.markdown(tem2, unsafe_allow_html=True)
 
     gender=st.radio("Select your gender", ['Male','Female'])
     if gender is 'Male':
         gender = 1
     else:
         gender = 0
-    height=st.number_input("Enter your height (cms)",value=0.1)
-    weight=st.number_input("Enter your weight (Kgs)")
+    height=st.number_input("Enter your height (cms)",value=164.35)
+    weight=st.number_input("Enter your weight (Kgs)", value = 74.20)
     height = height/100
     
     bmi = weight / (height ** 2)
@@ -44,12 +37,12 @@ def main():
         st.write(f"BMI calculated: ",round(bmi, 2))
     else:
         st.write("Please enter a valid height greater than zero.")
-    ap_hi=st.number_input("Enter your systolic blood pressure (ap_hi)")
-    ap_lo=st.number_input("Enter your diastolic blood pressure (ap_lo)")
+    ap_hi=st.number_input("Enter your systolic blood pressure (ap_hi)", value=128.81)
+    ap_lo=st.number_input("Enter your diastolic blood pressure (ap_lo)", value=96.63)
 
     map = ( ( 2 * ap_lo) + ap_hi ) / 3
  
-    years = st.number_input("Enter your age (Yrs)")
+    years = st.number_input("Enter your age (Yrs)", step = 1)
 
     if years >= 0 and years <= 20:
         age_bin =  0
@@ -131,7 +124,7 @@ def main():
         chol = 2
     else:
         chol = 3
-    gluc=st.radio("Select your Glucose Level (1-Normal, 2-Above Normal,3-Well Above Normal)", ['Normal','Above Normal','Well above Normal'])
+    gluc=st.radio("Select your Glucose Level", ['Normal','Above Normal','Well above Normal'])
     if gluc is 'Normal':
         gluc = 1
     elif gluc is 'Above Normal':
@@ -151,7 +144,7 @@ def main():
 
     inputs = [[gender,height,weight,bmi,ap_hi,ap_lo,map,years,age_bin,bmi_class,map_class,chol,gluc,smoke,active]]
 
-    if st.button('Predict'):#button name is Classify
+    if st.button('Predict'):#button name is Predict
         st.success(classify(random_model.predict(inputs)))
 
 if __name__ == "__main__":
